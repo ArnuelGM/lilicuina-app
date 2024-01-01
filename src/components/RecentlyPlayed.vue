79ac6da2c86cbea718b9c5896edf2de0dc60a075
Arnuel Gutierrez Menco <argume31@hotmail.com>
2024-01-01 18:19:38 -0500

    feat: :sparkles: RecentlyPlayed scroll navigation buttons

--- src/components/RecentlyPlayed.vue
+++ src/components/RecentlyPlayed.vue
@@ -1,34 +1,24 @@
 <style scoped>
-
-  .recently-played {
-    --sb-track-color: transparent;
-    --sb-thumb-color: rgb(4, 120, 87);
-    --sb-size: 2px;
-
-    scrollbar-color: var(--sb-thumb-color) 
-                    var(--sb-track-color);
-  }
-
   .recently-played::-webkit-scrollbar {
-    width: var(--sb-size);
-    height: var(--sb-size);
-  }
-
-  .recently-played::-webkit-scrollbar-track {
-    background: var(--sb-track-color);
-    border-radius: 10px;
-  }
-
-  .recently-played::-webkit-scrollbar-thumb {
-    background: var(--sb-thumb-color);
-    border-radius: 10px;
+    width: 0;
+    height: 0;
   }
 </style>
 <template>
   <section class="p-2">
-    <h2 class="font-bold text-xl mb-4">Recently Played</h2>
-    <ul class="flex flex-nowrap overflow-x-auto gap-4 recently-played snap-x snap-mandatory">
-      <li v-for="i in 10" class="w-32 h-40 rounded-lg overflow-hidden shrink-0 snap-start">
+    <div class="flex items-center justify-between mb-4">
+      <h2 class="font-bold text-xl">Recently Played</h2>
+      <div class="flex items-center gap-x-4">
+        <button @click="nextElement(-1)" aria-label="scroll left" class="text-black/60 dark:text-white/60">
+          <ChevronLeft />
+        </button>
+        <button @click="nextElement(1)" aria-label="scroll right" class="text-black/60 dark:text-white/60">
+          <ChevronRight />
+        </button>
+      </div>
+    </div>
+    <ul class="flex flex-nowrap overflow-x-auto gap-4 recently-played snap-x snap-mandatory" ref="slider">
+      <li v-for="i in 10" class="w-32 h-40 rounded-lg shrink-0 snap-start">
         <PopularItem />
       </li>
     </ul>
@@ -36,5 +26,15 @@
 </template>
 
 <script setup>
+  import { ref } from 'vue';
+  import ChevronLeft from '../Shared/icons/chevron-left.vue';
+  import ChevronRight from '../Shared/icons/chevron-right.vue';
   import PopularItem from './PopularItem.vue';
+
+  const slider = ref('slider')
+
+  function nextElement(direction = 1) {
+    const ITEM_WIDTH = slider.value.querySelector('li').clientWidth
+    slider.value.scrollBy({left: (ITEM_WIDTH * direction), behavior: 'smooth',  })
+  }
 </script>
